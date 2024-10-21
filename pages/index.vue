49f566a162e6a49bc75ff30b9d7c3cf053d82d8b
Marquis Abah <marquis.abah@shuttlers.ng>
2024-10-21 12:02:09 +0100

    fix build error

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
         </div> -->
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">MedLabConvo</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-300">Inspiring and building the capacity of future generations of world class professionals in medical laboratory science and in the healthcare system at large</p>
+          <p class="mt-6 text-lg leading-8 text-white">Inspiring and building the capacity of future generations of world class professionals in medical laboratory science and in the healthcare system at large</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="#" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">Get started</a>
             <a href="#" class="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
