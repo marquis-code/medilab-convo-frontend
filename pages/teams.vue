e66ef4381dc5c71fba39f8e1ede73ccbe1ae8455
Marquis <abahmarquis@gmail.com>
2025-01-04 13:09:18 -0800

    ipfate

--- pages/teams.vue
+++ pages/teams.vue
@@ -74,7 +74,7 @@
                 <dt class="text-base font-semibold leading-7 text-gray-900">
                   <div class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                     </svg>
                   </div>
                   Knowledge:
@@ -106,7 +106,7 @@
                 <dt class="text-base font-semibold leading-7 text-gray-900">
                   <div class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                     </svg>
                   </div>
                   Skill:
@@ -123,7 +123,7 @@
                 <dt class="text-base font-semibold leading-7 text-gray-900">
                   <div class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                     </svg>
                   </div>
                   Network:
@@ -140,7 +140,7 @@
                 <dt class="text-base font-semibold leading-7 text-gray-900">
                   <div class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                     </svg>
                   </div>
                   Identity:
@@ -386,70 +386,12 @@
         </div>
       </div>
       
-      <section>
+      <!-- <section>
         <img src="@/assets/img/organogram.png" alt="" />
-      </section>
-      
-    <div class="bg-white py-16 sm:py-20">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"></h2>
-            <p class="mt-6 text-lg leading-8 text-gray-600">We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.</p>
-          </div>
-          <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6">   
-            <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member1.jpg" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Operations Co-Lead</h3>
-                <p class="text-sm leading-6 text-gray-600">Sherifat Jimoh</p>
-              </li>
+      </section> -->
+    
 
-              <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member2.jpg" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Operations Co-Lead</h3>
-                <p class="text-sm leading-6 text-gray-600">Boluwatife Fatokun</p>
-              </li>
-
-              <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member3.jpg" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Volunteer Mgt Co-Lead</h3>
-                <p class="text-sm leading-6 text-gray-600">Victoria Omoloye</p>
-              </li>
-
-              <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member4.jpg" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Volunteer Mgt Co-Lead</h3>
-                <p class="text-sm leading-6 text-gray-600">Opemipo Adetayo</p>
-              </li>
-
-              <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member5.png" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Programs Co-Lead</h3>
-                <p class="text-sm leading-6 text-gray-600">Paul Yiran</p>
-              </li>
-
-              <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member6.jpg" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Programs Co-Lead</h3>
-                <p class="text-sm leading-6 text-gray-600">Glory Ogunbodede</p>
-              </li>
-
-              <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member7.jpg" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Fund. and Partners. Co-Lea</h3>
-                <p class="text-sm leading-6 text-gray-600">Emmanuel Anya-Awa</p>
-              </li>
-
-              <li>
-                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/member8.jpg" alt="">
-                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Fund. and Partners. Co-Lead</h3>
-                <p class="text-sm leading-6 text-gray-600">Oluwamuyiwa Ogunkoya</p>
-              </li>
-            <!-- More people... -->
-          </ul>
-        </div>
-      </div>
-
-      <div class="bg-white">
+      <div class="bg-white mt-32">
         <div class="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
           <div class="mx-auto max-w-2xl lg:mx-0">
             <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our team</h2>
@@ -476,8 +418,67 @@
       </div>
       
 
-     <section>
+      <div class="bg-white py-16 sm:py-20">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"></h2>
+            <p class="mt-6 text-lg leading-8 text-gray-600">We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.</p>
+          </div>
+          <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6">   
+            <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/sherifat.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Operations Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Sherifat Jimoh</p>
+              </li>
+
+              <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/boluwatife.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Operations Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Boluwatife Fatokun</p>
+              </li>
+
+              <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/chinwendu.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Volunteer Mgt Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Chinwendu Ezeagu</p>
+              </li>
+
+              <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/opemipo.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Volunteer Mgt Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Opemipo Adetayo</p>
+              </li>
+
+              <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/paul.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Programs Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Paul Yiran</p>
+              </li>
+
+              <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/glory.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Programs Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Glory Ogunbodede</p>
+              </li>
+
+              <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/joshua.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Fund. and Partners. Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Emmanuel Anya-Awa</p>
+              </li>
+
+              <li>
+                <img class="mx-auto h-24 w-24 rounded-full" src="@/assets/img/oluwamuyiwa.jpeg" alt="">
+                <h3 class="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">Fund. and Partners. Co-Lead</h3>
+                <p class="text-sm leading-6 text-gray-600">Oluwamuyiwa Ogunkoya</p>
+              </li>
+            <!-- More people... -->
+          </ul>
+        </div>
+      </div>
+
+     <!-- <section>
         <img src="@/assets/img/group-photo.png" alt="" />
-     </section>
+     </section> -->
 </main>
 </template>
