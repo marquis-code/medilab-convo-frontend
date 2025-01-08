77db5009ee319bf244b2e3a597c4b81c5cdab591
Marquis <abahmarquis@gmail.com>
2025-01-08 14:38:35 -0800

    updated code

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -8,20 +8,19 @@
             <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
               <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
             </div>
-            <div class="mx-auto max-w-7xl px-6 lg:px-8">
-              <div class="mx-auto max-w-2xl lg:mx-0">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8 flex justify-center items-center flex-col">
+              <div class="mx-auto max-w-2xl lg:mx-0 flex justify-center items-center flex-col">
                 <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Products</h2>
-                <p class="mt-6 text-lg leading-8 text-gray-300">Welcome to the MedLabConvo Products page, where we offer a diverse range of high-quality items designed to support your journey in medical laboratory science and beyond. Whether you're looking to enhance your knowledge, gear up for the lab, or show off your professional pride, we have something for everyone. </p>
+                <p class="mt-6 text-lg leading-8 text-gray-300 text-center">Welcome to the MedLabConvo Products page, where we offer a diverse range of high-quality items designed to support your journey in medical laboratory science and beyond. Whether you're looking to enhance your knowledge, gear up for the lab, or show off your professional pride, we have something for everyone. </p>
               </div>
             </div>
           </div>
 
-          <div class="bg-white pt-32">
+          <!-- <div class="bg-white pt-32">
             <div class="mx-auto max-w-7xl px-6 lg:px-8">
               <div class="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
                 <div class="w-full lg:max-w-lg lg:flex-auto">
                   <h2 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">LATEST RELEASE</h2>
-                  <!-- <p class="mt-6 text-xl leading-8 text-gray-600">Diam nunc lacus lacus aliquam turpis enim. Eget hac velit est euismod lacus. Est non placerat nam arcu. Cras purus nibh cursus sit eu in id.</p> -->
                   <img src="@/assets/img/breaking.jpg" alt="" class="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]">
                 </div>
                 <div class="w-full lg:max-w-xl lg:flex-auto">
@@ -43,31 +42,20 @@
                         <button class="bg-teal-600 w-full mt-5 text-white font-medium rounded-md px-8 py-3 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50">
                           Pre Order
                         </button>
-                        <!-- <dt class="sr-only">Salary</dt>
-                        <dd class="mt-4 text-base font-semibold leading-7 text-gray-900">$75,000 USD</dd>
-                        <dt class="sr-only">Location</dt>
-                        <dd class="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
-                          <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 flex-none fill-gray-300" aria-hidden="true">
-                            <circle cx="1" cy="1" r="1" />
-                          </svg>
-                          San Francisco, CA
-                        </dd> -->
                       </dl>
                     </li>
                   </ul>
-                  <!-- <div class="mt-8 flex border-t border-gray-100 pt-8">
-                    <a href="#" class="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">View all blog posts <span aria-hidden="true">&rarr;</span></a>
-                  </div> -->
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
           <div class="bg-white container mx-auto pt-16">
             <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-              <h2 class="text-xl font-bold text-gray-900">BOOKS</h2>
+              <h2 class="text-xl font-bold text-gray-900 text-center">BOOKS</h2>
           
-              <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-                <div>
+           <div class="flex justify-center items-center w-full">
+            <div class="mt-8 grid flex justify-center items-center w-full grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+                <div class="">
                   <div class="relative">
                     <div class="relative h-72 w-full overflow-hidden rounded-lg">
                       <img src="@/assets/img/book.jpg" alt="Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls." class="h-full w-full object-cover object-center">
@@ -82,11 +70,11 @@
                     </div>
                   </div>
                   <div class="mt-6">
-                    <a href="#" class="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200">Order</a>
+                    <a href="#" class="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200">Pre Order</a>
                   </div>
                 </div>
 
-                <div>
+                <!-- <div>
                   <div class="relative">
                     <div class="relative h-72 w-full overflow-hidden rounded-lg">
                       <img src="@/assets/img/book.jpg" alt="Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls." class="h-full w-full object-cover object-center">
@@ -122,19 +110,22 @@
                   <div class="mt-6">
                     <a href="#" class="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200">Order</a>
                   </div>
-                </div>
-          
-                <!-- More products... -->
+                </div> -->
               </div>
+           </div>
             </div>
           </div>
 
 
           <div class="bg-white container mx-auto pt-16">
-            <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-              <h2 class="text-xl font-bold text-gray-900">COURSES</h2>
+            <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl space-y-3 lg:px-8">
+              <h2 class="text-xl font-bold text-gray-900 text-center">COURSES</h2>
+              
+              <div class="border text-gray-400 rounded-lg border-[0.5px] py-20 text-center flex justify-center items-center">
+                COMING SOON
+              </div>
           
-              <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+              <!-- <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
                 <div>
                   <div class="relative">
                     <div class="relative h-72 w-full overflow-hidden rounded-lg">
@@ -192,16 +183,17 @@
                   </div>
                 </div>
           
-                <!-- More products... -->
-              </div>
+              </div> -->
             </div>
           </div>
 
           <div class="bg-white container mx-auto pt-16">
-            <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-              <h2 class="text-xl font-bold text-gray-900">LABCOATS & SCRUBS</h2>
-          
-              <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+            <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl space-y-3 lg:px-8">
+              <h2 class="text-xl font-bold text-gray-900 text-center">LABCOATS & SCRUBS</h2>
+              <div class="border text-gray-400 rounded-lg border-[0.5px] py-20 text-center flex justify-center items-center">
+                COMING SOON
+              </div>
+              <!-- <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
                 <div>
                   <div class="relative">
                     <div class="relative h-72 w-full overflow-hidden rounded-lg">
@@ -209,7 +201,6 @@
                     </div>
                     <div class="relative mt-4">
                       <h3 class="text-sm font-medium text-gray-900">Legit Scrubs</h3>
-                      <!-- <p class="mt-1 text-sm text-gray-500">by MLS Odinaka Obeta</p> -->
                     </div>
                     <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                       <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"></div>
@@ -228,7 +219,6 @@
                     </div>
                     <div class="relative mt-4">
                       <h3 class="text-sm font-medium text-gray-900">Chara Scrub</h3>
-                      <!-- <p class="mt-1 text-sm text-gray-500">by MLS Onaoluwa Abimbola</p> -->
                     </div>
                     <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                       <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"></div>
@@ -247,7 +237,6 @@
                     </div>
                     <div class="relative mt-4">
                       <h3 class="text-sm font-medium text-gray-900">MedXentials Lab Coat</h3>
-                      <!-- <p class="mt-1 text-sm text-gray-500">by MLS Elvis Umez</p> -->
                     </div>
                     <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                       <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"></div>
@@ -258,17 +247,17 @@
                     <a href="#" class="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200">Order</a>
                   </div>
                 </div>
-          
-                <!-- More products... -->
-              </div>
+              </div> -->
             </div>
           </div>
 
           <div class="bg-white container mx-auto pt-16">
-            <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-              <h2 class="text-xl font-bold text-gray-900">T-SHIRTS & OTHER ACCESSORIES</h2>
-          
-              <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+            <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl space-y-3 lg:px-8">
+              <h2 class="text-xl font-bold text-gray-900 text-center">T-SHIRTS & OTHER ACCESSORIES</h2>
+              <div class="border text-gray-400 rounded-lg border-[0.5px] py-20 text-center flex justify-center items-center">
+                COMING SOON
+              </div>
+              <!-- <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
                 <div>
                   <div class="relative">
                     <div class="relative h-72 w-full overflow-hidden rounded-lg">
@@ -276,7 +265,6 @@
                     </div>
                     <div class="relative mt-4">
                       <h3 class="text-sm font-medium text-gray-900">Dipstick series</h3>
-                      <!-- <p class="mt-1 text-sm text-gray-500">by MLS Odinaka Obeta</p> -->
                     </div>
                     <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                       <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"></div>
@@ -295,7 +283,6 @@
                     </div>
                     <div class="relative mt-4">
                       <h3 class="text-sm font-medium text-gray-900">+++ Grade series</h3>
-                      <!-- <p class="mt-1 text-sm text-gray-500">by MLS Onaoluwa Abimbola</p> -->
                     </div>
                     <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                       <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"></div>
@@ -314,7 +301,6 @@
                     </div>
                     <div class="relative mt-4">
                       <h3 class="text-sm font-medium text-gray-900">Vial Series</h3>
-                      <!-- <p class="mt-1 text-sm text-gray-500">by MLS Elvis Umez</p> -->
                     </div>
                     <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                       <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"></div>
@@ -325,9 +311,7 @@
                     <a href="#" class="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200">Order</a>
                   </div>
                 </div>
-          
-                <!-- More products... -->
-              </div>
+              </div> -->
             </div>
           </div>
     </main>    
