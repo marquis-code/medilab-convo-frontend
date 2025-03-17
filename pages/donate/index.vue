74704332b072b3042b9326db346afbb91ff62952
Prisca Onyemaechi <priscaonyemaechi26@gmail.com>
2025-03-17 09:11:44 +0100

    Added donate page

---
+++ pages/donate/index.vue
@@ -0,0 +1,206 @@
+<template>
+  <main>
+    <div class="bg-[#27628C]">
+      <div class="relative isolate overflow-hidden pt-14">
+        <img src="@/assets/img/hero-background.jpeg" alt="" class="absolute inset-0 -z-10 w-full object-cover" />
+        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true">
+          <div
+            class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style="
+              clip-path: polygon(
+                74.1% 44.1%,
+                100% 61.6%,
+                97.5% 26.9%,
+                85.5% 0.1%,
+                80.7% 2%,
+                72.5% 32.5%,
+                60.2% 62.4%,
+                52.4% 68.1%,
+                47.5% 58.3%,
+                45.2% 34.5%,
+                27.5% 76.7%,
+                0.1% 64.9%,
+                17.9% 100%,
+                27.6% 76.8%,
+                76.1% 97.7%,
+                74.1% 44.1%
+              );
+            "></div>
+        </div>
+        <div class="mx-auto max-w-2xl lg:py-32 py-28">
+          <div class="text-center space-y-3">
+            <h1 class="font-bold tracking-tight text-white text-5xl">
+              Support MedLabConvo
+            </h1>
+            <p class="text-3xl leading-8 text-white italic px-2 lg:px-0">
+              Empower the Future of Medical Laboratory Science
+            </p>
+          </div>
+        </div>
+        <div
+          class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true">
+          <div
+            class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style="
+              clip-path: polygon(
+                74.1% 44.1%,
+                100% 61.6%,
+                97.5% 26.9%,
+                85.5% 0.1%,
+                80.7% 2%,
+                72.5% 32.5%,
+                60.2% 62.4%,
+                52.4% 68.1%,
+                47.5% 58.3%,
+                45.2% 34.5%,
+                27.5% 76.7%,
+                0.1% 64.9%,
+                17.9% 100%,
+                27.6% 76.8%,
+                76.1% 97.7%,
+                74.1% 44.1%
+              );
+            "></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-col space-y-4 max-w-sm mx-auto pt-4 mt-20">
+      <a href="https://paystack.com/pay/medlabconvodonation" target="_blank"
+        class="bg-[#27628C] text-center text-white py-3 px-3 rounded transition-colors hover:bg-[#1a4665]">
+        Donate Now
+      </a>
+    </div>
+
+    <div class="mt-20 flex justify-center items-center px-4 mt-32">
+      <div class="max-w-[900px] w-full flex flex-col justify-center items-center">
+        <h2 class="text-2xl md:text-4xl text-center font-extrabold text-gray-900">
+          Why Donate to MedLabConvo?
+        </h2>
+        <p class="text-xl md:text-2xl max-w-[800px] w-full text-gray-900 text-center mt-">
+          MedLabConvo is more than just a platform; it is a movement dedicated
+          to transforming the landscape of medical laboratory science. Through
+          education, mentorship, advocacy, and system strengthening, we are
+          shaping the future of healthcare by empowering laboratory
+          professionals at every stage of their careers.
+        </p>
+      </div>
+    </div>
+
+    <div class="bg-white container mx-auto mt-32">
+      <div class="mx-auto px-4 sm:px-6 lg:px-4">
+        <h2 class="text-2xl font-bold text-gray-900 text-center uppercase">
+          Your donation helps us:
+        </h2>
+
+        <div class="flex justify-center items-center w-full lg:flex pt-10">
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-14">
+            <div
+              class="max-w-md lg:max-w-lg lg:w-[500px] w-[300px] mx-auto bg-white rounded-lg border-2 border-gray-200 shadow-sm hover:shadow-md transition-shadow overflow-hidden p-6">
+              <div class="space-y-4">
+                <h1 class="text-xl font-semibold text-gray-500 uppercase tracking-wider">
+                  ✅ Train and Mentor Future Leaders
+                </h1>
+                <p class="text-xl text-gray-900 tracking-tighter mb-2">
+                  Our Origins Program and professional development initiatives
+                  equip young medical laboratory scientists with the skills and
+                  opportunities they need to thrive.
+                </p>
+              </div>
+            </div>
+
+            <div
+              class="max-w-md lg:max-w-lg lg:w-[500px] w-[300px] mx-auto bg-white rounded-lg border-2 border-gray-200 shadow-sm hover:shadow-md transition-shadow overflow-hidden p-6">
+              <div class="space-y-4">
+                <h1 class="text-xl font-semibold text-gray-500 uppercase tracking-wider">
+                  ✅ Bridge the Knowledge Gap
+                </h1>
+                <p class="text-xl text-gray-900 tracking-tighter mb-2">
+                  Through our LabCast podcast, blogs, and research resources, we
+                  provide critical knowledge that enhances clinical diagnostics
+                  and patient care.
+                </p>
+              </div>
+            </div>
+
+            <div
+              class="max-w-md lg:max-w-lg lg:w-[500px] w-[300px] mx-auto bg-white rounded-lg border-2 border-gray-200 shadow-sm hover:shadow-md transition-shadow overflow-hidden p-6">
+              <div class="space-y-4">
+                <h1 class="text-xl font-semibold text-gray-500 uppercase tracking-wider">
+                  ✅ Strengthen Healthcare Systems
+                </h1>
+                <p class="text-xl text-gray-900 tracking-tighter mb-2">
+                  By fostering collaboration and advocating for laboratory
+                  medicine, we contribute to a more robust and efficient
+                  healthcare system.
+                </p>
+              </div>
+            </div>
+
+            <div
+              class="max-w-md lg:max-w-lg lg:w-[500px] w-[300px] mx-auto bg-white rounded-lg border-2 border-gray-200 shadow-sm hover:shadow-md transition-shadow overflow-hidden p-6">
+              <div class="space-y-4">
+
+
+                <h1 class="text-xl font-semibold text-gray-500 uppercase tracking-wider">
+                  ✅ Expand Access to Opportunities
+                </h1>
+                <p class="text-xl text-gray-900 tracking-tighter mb-2">
+                  Our networking and community-building efforts connect medical
+                  laboratory scientists worldwide, ensuring that no one is left
+                  behind.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-[#27628C] w-full  flex justify-center items-center py-20 px-4 mt-32">
+  <div class="max-w-[900px] w-full flex flex-col justify-center items-center text-white">
+    <h2 class="text-2xl text-center font-bold uppercase">
+      Where Your Donations Go
+    </h2>
+    <p class="text-xl md:text-2xl max-w-[800px] w-full text-center mt-6">
+      Every contribution, big or small, directly fuels our mission. Your donations will support:
+    </p>
+    <ul class="mt-6 text-lg md:text-xl list-disc list-inside space-y-3">
+      <li>The development of educational content, workshops, and training sessions</li>
+      <li>The expansion of our mentorship and internship support programs</li>
+      <li>The sustainability of LabCast, Journo, and other knowledge-sharing platforms</li>
+      <li>The launch of new initiatives aimed at health advocacy and system reform</li>
+    </ul>
+  </div>
+</div>
+
+
+
+    <div class="flex justify-center items-center px-4 mt-32">
+  <div class="max-w-[900px] w-full flex flex-col justify-center items-center">
+    <h2 class="text-balance text-3xl px-3 lg:px-0 lg:text-4xl text-center font-semibold tracking-tight text-gray-900">
+      Join Us in Making an Impact
+    </h2>
+    <p class="text-xl md:text-2xl max-w-[800px] w-full text-gray-900 text-center mt-6">
+      By supporting MedLabConvo, you are investing in a future where medical laboratory scientists are better
+      equipped, more connected, and more empowered to drive change in healthcare.
+    </p>
+    <p class="mt-2 text-balance text-3xl px-3 lg:px-0 lg:text-4xl pt-20 text-center font-semibold tracking-tight text-gray-900">
+      Donate Today and Be Part of the Change!
+    </p>
+  </div>
+</div>
+
+    <div class="flex flex-col space-y-4 max-w-sm mx-auto pt-4 mt-6 mb-20">
+      <a
+        href="https://paystack.com/pay/medlabconvodonation"
+        target="_blank"
+        class="bg-[#27628C] text-center text-white py-3 px-3 rounded transition-colors hover:bg-[#1a4665]"
+      >
+        Donate Now
+      </a>
+    </div>
+  </main>
+</template>
