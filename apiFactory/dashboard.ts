899d4568919542dc9232d44243d2b02b93c0cc10
Marquis <abahmarquis@gmail.com>
2025-03-30 13:55:48 +0100

    anonymous

--- apiFactory/dashboard.ts
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from "./axios.config";
-
-export const dashboardApiFactory = {
-  getDashboardSummary() {
-    return axiosInstance.get('/user/dashboard-summary');
-  },
-};
--- apiFactory/files.ts
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from "./axios.config";
-
-export const filesApiFactory = {
-  getFilesList() {
-    return axiosInstance.get("/user/dashboard-summary");
-  },
-};
