d8df7a5baf243c17f5c130b527bc92e04ab0eab7
marquis-code <abahmarquis@gmail.com>
2025-09-20 10:04:11 +0100

    fixed

---
+++ pages/play.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/rhVkU6HAbn0?si=PP3LqgBgNf_oVTMa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </main>
+</template>
