d84b1e28d40309df6427e3ef56455c58478e3f9c
Prisca Onyemaechi <priscaonyemaechi26@gmail.com>
2026-01-17 11:29:44 +0100

    corrected an error

--- pages/programs/[id].vue
+++ pages/programs/[id].vue
@@ -119,7 +119,7 @@ const schedules = ref([
   {
     image: speaker1,
     "date_and_time": "Friday, 30th January, 2026, 8 pm - 9 pm",
-"topic": "The Degree Delusion: Why Your Degree is Not Enough and How To Win Before You Graduate,
+"topic": "The Degree Delusion: Why Your Degree is Not Enough and How To Win Before You Graduate",
     "speaker": "Oluwamuyiwa Ogunkoya",
     "bio": `
  Oluwamuyiwa Ogunkoya is a Medical Laboratory Scientist with
